3e0b050c42b3e9f9ba7310eaba77480ac4943cc3
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-12-28 22:16:30 -0400

    64 bits sld2 and glad combo

--- src/main.cpp
+++ src/main.cpp
@@ -1,413 +1,119 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
-and may not be redistributed without written permission.*/
+// On linux compile with:
+// g++ -std=c++17 main.cpp glad/src/glad.c -I./glad/include -o prog -lSDL2 -ldl
+// On windows compile with (if using mingw)
+// g++ main.cpp ./glad/src/glad.c -I./glad/include -o prog.exe -lmingw32 -lSDL2main -lSDL2
 
-//Using SDL, SDL OpenGL, GLEW, standard IO, and strings
+// C++ Standard Libraries
+#include <iostream>
+
+// Third-party library
 #include <SDL2/SDL.h>
-#include <GL/glew.h>
-// #include <SDL2/SDL_opengl.h>
-#include <GL/glu.h>
-#include <stdio.h>
-#include <string>
+// Include GLAD
+#include <glad/glad.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+int main(int argc, char* argv[]){
 
-//Starts up SDL, creates window, and initializes OpenGL
-bool init();
+    // Create a window data type
+    // This pointer will point to the 
+    // window that is allocated from SDL_CreateWindow
+    SDL_Window* window=nullptr;
 
-//Initializes rendering program and clear color
-bool initGL();
+    // Initialize the video subsystem.
+    // If it returns less than 1, then an
+    // error code will be received.
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        std::cout << "SDL could not be initialized: " <<
+                  SDL_GetError();
+    }else{
+        std::cout << "SDL video system is ready to go\n";
+    }
+    // Before we create our window, specify OpenGL version
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-//Input handler
-void handleKeys( unsigned char key, int x, int y );
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,24);
 
-//Per frame update
-void update();
+    // Request a window to be created for our platform
+    // The parameters are for the title, x and y position,
+    // and the width and height of the window.
+    window = SDL_CreateWindow("C++ SDL2 Window",
+            20,
+            20,
+            640,
+            480,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
-//Renders quad to the screen
-void render();
+    // OpenGL setup the graphics context
+    SDL_GLContext context;
+    context = SDL_GL_CreateContext(window);
 
-//Frees media and shuts down SDL
-void close();
+    // Setup our function pointers
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
 
-//Shader loading utility programs
-void printProgramLog( GLuint program );
-void printShaderLog( GLuint shader );
+    // Infinite loop for our application
+    bool gameIsRunning = true;
+    while(gameIsRunning){
+        glViewport(0,0,640,480);
 
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
+        SDL_Event event;
+        // Start our event loop
+        while(SDL_PollEvent(&event)){
+            // Handle each specific event
+            if(event.type == SDL_QUIT){
+                gameIsRunning= false;
+            }
+            if(event.type == SDL_MOUSEMOTION){
+                std::cout << "mouse has been moved\n";
+            }
+            if(event.type == SDL_KEYDOWN){
+                std::cout << "a key has been pressed\n";
+                if(event.key.keysym.sym == SDLK_0){
+                    std::cout << "0 was pressed\n";
+                }else{
+                    std::cout << "0 was not pressed\n";
+                }
+            }
+            // Retrieve the state of all of the keys
+            // Then we can query the scan code of one or more
+            // keys at a time
+            const Uint8* state = SDL_GetKeyboardState(NULL);
+            if(state[SDL_SCANCODE_RIGHT]){
+                std::cout << "right arrow key is pressed\n";
+            }
+        }
 
-//OpenGL context
-SDL_GLContext gContext;
+        glClearColor(1.0f,0.0f,0.0f,1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-//Render flag
-bool gRenderQuad = true;
+        SDL_GL_SwapWindow(window);
+    }
 
-//Graphics program
-GLuint gProgramID = 0;
-GLint gVertexPos2DLocation = -1;
-GLuint gVBO = 0;
-GLuint gIBO = 0;
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Use OpenGL 3.1 core
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
-
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create context
-			gContext = SDL_GL_CreateContext( gWindow );
-			if( gContext == NULL )
-			{
-				printf( "OpenGL context could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize GLEW
-				glewExperimental = GL_TRUE; 
-				GLenum glewError = glewInit();
-				if( glewError != GLEW_OK )
-				{
-					printf( "Error initializing GLEW! %s\n", glewGetErrorString( glewError ) );
-				}
-
-				//Use Vsync
-				if( SDL_GL_SetSwapInterval( 1 ) < 0 )
-				{
-					printf( "Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError() );
-				}
-
-				//Initialize OpenGL
-				if( !initGL() )
-				{
-					printf( "Unable to initialize OpenGL!\n" );
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
-}
-
-bool initGL()
-{
-	//Success flag
-	bool success = true;
-
-	//Generate program
-	gProgramID = glCreateProgram();
-
-	//Create vertex shader
-	GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
-
-	//Get vertex source
-	const GLchar* vertexShaderSource[] =
-	{
-		"#version 140\nin vec2 LVertexPos2D; void main() { gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 ); }"
-	};
-
-	//Set vertex source
-	glShaderSource( vertexShader, 1, vertexShaderSource, NULL );
-
-	//Compile vertex source
-	glCompileShader( vertexShader );
-
-	//Check vertex shader for errors
-	GLint vShaderCompiled = GL_FALSE;
-	glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &vShaderCompiled );
-	if( vShaderCompiled != GL_TRUE )
-	{
-		printf( "Unable to compile vertex shader %d!\n", vertexShader );
-		printShaderLog( vertexShader );
-        success = false;
-	}
-	else
-	{
-		//Attach vertex shader to program
-		glAttachShader( gProgramID, vertexShader );
-
-
-		//Create fragment shader
-		GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-
-		//Get fragment source
-		const GLchar* fragmentShaderSource[] =
-		{
-			"#version 140\nout vec4 LFragment; void main() { LFragment = vec4( 1.0, 1.0, 1.0, 1.0 ); }"
-		};
-
-		//Set fragment source
-		glShaderSource( fragmentShader, 1, fragmentShaderSource, NULL );
-
-		//Compile fragment source
-		glCompileShader( fragmentShader );
-
-		//Check fragment shader for errors
-		GLint fShaderCompiled = GL_FALSE;
-		glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &fShaderCompiled );
-		if( fShaderCompiled != GL_TRUE )
-		{
-			printf( "Unable to compile fragment shader %d!\n", fragmentShader );
-			printShaderLog( fragmentShader );
-			success = false;
-		}
-		else
-		{
-			//Attach fragment shader to program
-			glAttachShader( gProgramID, fragmentShader );
-
-
-			//Link program
-			glLinkProgram( gProgramID );
-
-			//Check for errors
-			GLint programSuccess = GL_TRUE;
-			glGetProgramiv( gProgramID, GL_LINK_STATUS, &programSuccess );
-			if( programSuccess != GL_TRUE )
-			{
-				printf( "Error linking program %d!\n", gProgramID );
-				printProgramLog( gProgramID );
-				success = false;
-			}
-			else
-			{
-				//Get vertex attribute location
-				gVertexPos2DLocation = glGetAttribLocation( gProgramID, "LVertexPos2D" );
-				if( gVertexPos2DLocation == -1 )
-				{
-					printf( "LVertexPos2D is not a valid glsl program variable!\n" );
-					success = false;
-				}
-				else
-				{
-					//Initialize clear color
-					glClearColor( 0.f, 0.f, 0.f, 1.f );
-
-					//VBO data
-					GLfloat vertexData[] =
-					{
-						-0.5f, -0.5f,
-						 0.5f, -0.5f,
-						 0.5f,  0.5f,
-						-0.5f,  0.5f
-					};
-
-					//IBO data
-					GLuint indexData[] = { 0, 1, 2, 3 };
-
-					//Create VBO
-					glGenBuffers( 1, &gVBO );
-					glBindBuffer( GL_ARRAY_BUFFER, gVBO );
-					glBufferData( GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), vertexData, GL_STATIC_DRAW );
-
-					//Create IBO
-					glGenBuffers( 1, &gIBO );
-					glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, gIBO );
-					glBufferData( GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), indexData, GL_STATIC_DRAW );
-				}
-			}
-		}
-	}
-	
-	return success;
-}
-
-void handleKeys( unsigned char key, int x, int y )
-{
-	//Toggle quad
-	if( key == 'q' )
-	{
-		gRenderQuad = !gRenderQuad;
-	}
-}
-
-void update()
-{
-	//No per frame update needed
-}
-
-void render()
-{
-	//Clear color buffer
-	glClear( GL_COLOR_BUFFER_BIT );
-	
-	//Render quad
-	if( gRenderQuad )
-	{
-		//Bind program
-		glUseProgram( gProgramID );
-
-		//Enable vertex position
-		glEnableVertexAttribArray( gVertexPos2DLocation );
-
-		//Set vertex data
-		glBindBuffer( GL_ARRAY_BUFFER, gVBO );
-		glVertexAttribPointer( gVertexPos2DLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL );
-
-		//Set index data and render
-		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, gIBO );
-		glDrawElements( GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL );
-
-		//Disable vertex position
-		glDisableVertexAttribArray( gVertexPos2DLocation );
-
-		//Unbind program
-		glUseProgram( NULL );
-	}
-}
-
-void close()
-{
-	//Deallocate program
-	glDeleteProgram( gProgramID );
-
-	//Destroy window	
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-
-	//Quit SDL subsystems
-	SDL_Quit();
-}
-
-void printProgramLog( GLuint program )
-{
-	//Make sure name is shader
-	if( glIsProgram( program ) )
-	{
-		//Program log length
-		int infoLogLength = 0;
-		int maxLength = infoLogLength;
-		
-		//Get info string length
-		glGetProgramiv( program, GL_INFO_LOG_LENGTH, &maxLength );
-		
-		//Allocate string
-		char* infoLog = new char[ maxLength ];
-		
-		//Get info log
-		glGetProgramInfoLog( program, maxLength, &infoLogLength, infoLog );
-		if( infoLogLength > 0 )
-		{
-			//Print Log
-			printf( "%s\n", infoLog );
-		}
-		
-		//Deallocate string
-		delete[] infoLog;
-	}
-	else
-	{
-		printf( "Name %d is not a program\n", program );
-	}
-}
-
-void printShaderLog( GLuint shader )
-{
-	//Make sure name is shader
-	if( glIsShader( shader ) )
-	{
-		//Shader log length
-		int infoLogLength = 0;
-		int maxLength = infoLogLength;
-		
-		//Get info string length
-		glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &maxLength );
-		
-		//Allocate string
-		char* infoLog = new char[ maxLength ];
-		
-		//Get info log
-		glGetShaderInfoLog( shader, maxLength, &infoLogLength, infoLog );
-		if( infoLogLength > 0 )
-		{
-			//Print Log
-			printf( "%s\n", infoLog );
-		}
-
-		//Deallocate string
-		delete[] infoLog;
-	}
-	else
-	{
-		printf( "Name %d is not a shader\n", shader );
-	}
-}
-
-int main( int argc, char* args[] )
-{
-	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Main loop flag
-		bool quit = false;
-
-		//Event handler
-		SDL_Event e;
-		
-		//Enable text input
-		SDL_StartTextInput();
-
-		//While application is running
-		while( !quit )
-		{
-			//Handle events on queue
-			while( SDL_PollEvent( &e ) != 0 )
-			{
-				//User requests quit
-				if( e.type == SDL_QUIT )
-				{
-					quit = true;
-				}
-				//Handle keypress with current mouse position
-				else if( e.type == SDL_TEXTINPUT )
-				{
-					int x = 0, y = 0;
-					SDL_GetMouseState( &x, &y );
-					handleKeys( e.text.text[ 0 ], x, y );
-				}
-			}
-
-			//Render quad
-			render();
-			
-			//Update screen
-			SDL_GL_SwapWindow( gWindow );
-		}
-		
-		//Disable text input
-		SDL_StopTextInput();
-	}
-
-	//Free resources and close SDL
-	close();
-
-	return 0;
+    // We destroy our window. We are passing in the pointer
+    // that points to the memory allocated by the 
+    // 'SDL_CreateWindow' function. Remember, this is
+    // a 'C-style' API, we don't have destructors.
+    SDL_DestroyWindow(window);
+    
+    // We safely uninitialize SDL2, that is, we are
+    // taking down the subsystems here before we exit
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // our program.
+    SDL_Quit();
+    return 0;
 }
