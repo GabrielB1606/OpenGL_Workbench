85842dea326368af0d4fab4ee1d413203cac47ce
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-01-03 12:00:02 -0400

    ImGui integration

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@
 // Include GLAD
 #include <glad/glad.h>
 
+#include <imgui.h>
+#include <imgui_impl_sdl.h>
+#include <imgui_impl_opengl3.h>
+
 int main(int argc, char* argv[]){
 
     // Create a window data type
@@ -52,6 +56,25 @@ int main(int argc, char* argv[]){
     // Setup our function pointers
     gladLoadGLLoader(SDL_GL_GetProcAddress);
 
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(window, context);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
+    bool show_demo_window = true;
+    bool show_another_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     // Infinite loop for our application
     bool gameIsRunning = true;
     while(gameIsRunning){
@@ -60,13 +83,16 @@ int main(int argc, char* argv[]){
         SDL_Event event;
         // Start our event loop
         while(SDL_PollEvent(&event)){
+
+            ImGui_ImplSDL2_ProcessEvent(&event);
+
             // Handle each specific event
             if(event.type == SDL_QUIT){
                 gameIsRunning= false;
             }
-            if(event.type == SDL_MOUSEMOTION){
-                std::cout << "mouse has been moved\n";
-            }
+            // if(event.type == SDL_MOUSEMOTION){
+            //     std::cout << "mouse has been moved\n";
+            // }
             if(event.type == SDL_KEYDOWN){
                 std::cout << "a key has been pressed\n";
                 if(event.key.keysym.sym == SDLK_0){
@@ -84,11 +110,66 @@ int main(int argc, char* argv[]){
             }
         }
 
-        glClearColor(1.0f,0.0f,0.0f,1.0f);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
 
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
+
+        // glClearColor(1.0f,0.0f,0.0f,1.0f);
+        // glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        // SDL_GL_SwapWindow(window);
     }
+
+    // ImGui cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 
     // We destroy our window. We are passing in the pointer
     // that points to the memory allocated by the 
@@ -98,22 +179,6 @@ int main(int argc, char* argv[]){
     
     // We safely uninitialize SDL2, that is, we are
     // taking down the subsystems here before we exit
-    // We add a delay in order to see that our window
-    // has successfully popped up.
-    SDL_Delay(3000);
-    // We add a delay in order to see that our window
-    // has successfully popped up.
-    SDL_Delay(3000);
-    // We add a delay in order to see that our window
-    // has successfully popped up.
-    SDL_Delay(3000);
-    // We add a delay in order to see that our window
-    // has successfully popped up.
-    SDL_Delay(3000);
-    // We add a delay in order to see that our window
-    // has successfully popped up.
-    SDL_Delay(3000);
-    // our program.
     SDL_Quit();
     return 0;
 }
