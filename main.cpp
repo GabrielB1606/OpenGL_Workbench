ae5ab76837793a4a13756127037aeecd3409213a
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-02-17 08:32:44 -0400

    hello triangle

--- main.cpp
+++ main.cpp
@@ -1,241 +1,165 @@
-// On linux compile with:
-// g++ -std=c++17 main.cpp glad/src/glad.c -I./glad/include -o prog -lSDL2 -ldl
-// On windows compile with (if using mingw)
-// g++ main.cpp ./glad/src/glad.c -I./glad/include -o prog.exe -lmingw32 -lSDL2main -lSDL2
-#include <headers.h>
+#include <iostream>
 
-int main(int argc, char* argv[]){
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-    // Create a window data type
-    // This pointer will point to the 
-    // window that is allocated from SDL_CreateWindow
-    SDL_Window* window=nullptr;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
-    // Initialize the video subsystem.
-    // If it returns less than 1, then an
-    // error code will be received.
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        std::cout << "SDL could not be initialized: " << SDL_GetError();
-    }else{
-        std::cout << "SDL video system is ready to go\n";
-    }
-    // Before we create our window, specify OpenGL version
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+// Variables for checking compilation/linking correctness
+int success;
+char infoLog[512];
 
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,24);
+// Basic vertex shader in GLSL (OpenGL Shading Language)
+const char *vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
 
-    // Request a window to be created for our platform
-    // The parameters are for the title, x and y position,
-    // and the width and height of the window.
-    window = SDL_CreateWindow("C++ SDL2 Window",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            1280,
-            720,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+// Basic fragment shader
+const char *fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
 
-    // OpenGL setup the graphics context
-    SDL_GLContext context;
-    context = SDL_GL_CreateContext(window);
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Setup our function pointers
-    gladLoadGLLoader(SDL_GL_GetProcAddress);
-    // gladLoadGL();
+	// 1. Build and compile our shader programs
 
-    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
-    printf("Renderer: %s\n", glGetString(GL_RENDERER));
-    printf("Version:  %s\n", glGetString(GL_VERSION));
-    
-    // Use v-sync
-    SDL_GL_SetSwapInterval(1);
+	// Create a vertex shader object
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-    // // Setup Dear ImGui context
-    // IMGUI_CHECKVERSION();
-    // ImGui::CreateContext();
-    // ImGuiIO& io = ImGui::GetIO(); (void)io;
-    // //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    // //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	// Attach the shader source code to the shader object
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 
-    // // Setup Dear ImGui style
-    // ImGui::StyleColorsDark();
-    // //ImGui::StyleColorsLight();
+	// Compile the vertex shader dynamically 
+	glCompileShader(vertexShader);
 
-    // // Setup Platform/Renderer backends
-    // ImGui_ImplSDL2_InitForOpenGL(window, context);
-    // ImGui_ImplOpenGL3_Init("#version 460");
+	// Check if compilation was successful
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n";
+		std::cout << infoLog << std::endl;
+	}
 
-    glViewport(0, 0, 1280, 720);
-    glDisable(GL_CULL_FACE);
-    glDisable(GL_DEPTH_TEST);
-    // bool show_demo_window = true;
-    // bool show_another_window = true;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	// Create a fragment shader object
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-    Program p( std::string("460").c_str() , 4, 6, "shaders/core/vertex.vert", "shaders/core/fragment.frag", nullptr );
-    
-    // hello triangle
+	// Attach the shader source code to the shader object
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 
-	// Vertices coordinates
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	// Compile the fragment shader dynamically
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n";
+		std::cout << infoLog << std::endl;
+	}
+
+
+	// 2. Link shaders
+
+	// Create a shader program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	// Attach the compiled shaders to the shader program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Check if linking was successful
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::PROGRAM::LINKING_FAILED\n";
+		std::cout << infoLog << std::endl;
+	}
+
+	// Delete shader objects if we no longer need them anymore
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	// 3. Set up vertex data and configure vertex attributes
+
+	// Define three vertices with 3D positions
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
 	};
 
-	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
-	GLuint VAO, VBO;
-
-	// Generate the VAO and VBO with only 1 object each
+	// Generate vertex buffer object (VBO) and vertex array object (VAO)
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
-	// Make the VAO the current Vertex Array Object by binding it
+	// Bind VAO, then bind VBO
 	glBindVertexArray(VAO);
-
-	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Introduce the vertices into the VBO
+
+	// Copy the vertex data into the buffer's memory
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+	// Set attributes that describe how OpenGL should interpret the vertex data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	// Enable the Vertex Attribute so that OpenGL knows to use it
 	glEnableVertexAttribArray(0);
 
-	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+
+	// Unbind so other calls won't modify VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
 
-    // Infinite loop for our application
-    bool gameIsRunning = true;
-    while(gameIsRunning){
-
-        // Specify the color of the background
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color to it
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		p.use();
-		// Bind the VAO so OpenGL knows to use it
+
+		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		// Draw the triangle using the GL_TRIANGLES primitive
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// Swap the back buffer with the front buffer
-		SDL_GL_SwapWindow(window);
 
-        SDL_Event event;
-        // Start our event loop
-        while(SDL_PollEvent(&event)){
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
-            // ImGui_ImplSDL2_ProcessEvent(&event);
+	glfwTerminate();
 
-            // Handle each specific event
-            if(event.type == SDL_QUIT){
-                gameIsRunning= false;
-            }
-            // if(event.type == SDL_MOUSEMOTION){
-            //     std::cout << "mouse has been moved\n";
-            // }
-            if(event.type == SDL_KEYDOWN){
-                std::cout << "a key has been pressed\n";
-                if(event.key.keysym.sym == SDLK_0){
-                    std::cout << "0 was pressed\n";
-                }else{
-                    std::cout << "0 was not pressed\n";
-                }
-            }
-            // Retrieve the state of all of the keys
-            // Then we can query the scan code of one or more
-            // keys at a time
-            const Uint8* state = SDL_GetKeyboardState(NULL);
-            if(state[SDL_SCANCODE_RIGHT]){
-                std::cout << "right arrow key is pressed\n";
-            }
-        }
+	return 0;
+}
 
-        // Start the Dear ImGui frame
-        // ImGui_ImplOpenGL3_NewFrame();
-        // ImGui_ImplSDL2_NewFrame();
-        // ImGui::NewFrame();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
 
-        // // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        // if (show_demo_window)
-        //     ImGui::ShowDemoWindow(&show_demo_window);
-
-        // // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        // {
-        //     static float f = 0.0f;
-        //     static int counter = 0;
-
-        //     ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        //     ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        //     ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        //     ImGui::Checkbox("Another Window", &show_another_window);
-
-        //     ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        //     ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        //     if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-        //         counter++;
-        //     ImGui::SameLine();
-        //     ImGui::Text("counter = %d", counter);
-
-        //     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        //     ImGui::End();
-        // }
-
-        // // 3. Show another simple window.
-        // if (show_another_window)
-        // {
-        //     ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        //     ImGui::Text("Hello from another window!");
-        //     if (ImGui::Button("Close Me"))
-        //         show_another_window = false;
-        //     ImGui::End();
-        // }
-
-        // // Rendering
-        // ImGui::Render();
-        // glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        // glClear(GL_COLOR_BUFFER_BIT);
-
-        // p.use();
-        // glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
-        // p.stopUsing();
-
-        // // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        // SDL_GL_SwapWindow(window);
-
-        // glClearColor(1.0f,0.0f,0.0f,1.0f);
-        // glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-        // SDL_GL_SwapWindow(window);
-    }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-
-    // ImGui cleanup
-    // ImGui_ImplOpenGL3_Shutdown();
-    // ImGui_ImplSDL2_Shutdown();
-    // ImGui::DestroyContext();
-
-    // We destroy our window. We are passing in the pointer
-    // that points to the memory allocated by the 
-    // 'SDL_CreateWindow' function. Remember, this is
-    // a 'C-style' API, we don't have destructors.
-    SDL_DestroyWindow(window);
-    
-    // We safely uninitialize SDL2, that is, we are
-    // taking down the subsystems here before we exit
-    SDL_Quit();
-    return 0;
+void processInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
