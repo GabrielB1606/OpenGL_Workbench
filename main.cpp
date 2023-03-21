5264e874916aca1b94c4c673c475022655a16846
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-03-20 22:47:43 -0400

    depth testing fix

--- main.cpp
+++ main.cpp
@@ -7,47 +7,43 @@ glm::mat4 projection = glm::mat4(1.f);
 // Define the projection matrix
 float fov = 90.0f;  // Field of view in degrees
 float aspectRatio = 1280.0f / 720.0f; // Aspect ratio of the window
-float nearPlane = 1.f;  // Near clipping plane
-float farPlane = 99.0f; // Far clipping plane
+float nearPlane = 0.1f;  // Near clipping plane
+float farPlane = 1000.0f; // Far clipping plane
+
+void configOpenGL();
 
 int main() {
 	
+	// create the window
 	WindowManager windowManager(1280, 720, "window manager", glMajVersion, glMinVersion);
-
-	// 1. Build and compile our shader programs
+	// setup the graphics pipeline with OpenGL
 	ShaderProgram s("460", glMajVersion, glMinVersion, "shaders/core/vertex.vert", "shaders/core/fragment.frag");
+	// initial configuration
+	configOpenGL();
 
-
+	// initial projection matrix
 	projection = glm::perspective(glm::radians(fov), aspectRatio, nearPlane, farPlane);
+	s.setMat4fv(projection, "ProjectionMatrix", GL_FALSE);	
 
-	s.setMat4fv(projection, "ProjectionMatrix", GL_FALSE);
-
-	glDisable(GL_CULL_FACE);
-	// glEnable(GL_CULL_FACE);
-	// glFrontFace(GL_CW);
-	// glCullFace(GL_BACK);
-	// glEnable(GL_DEPTH_TEST);
-
-	BasicMesh* mesh = new BasicMesh();
-	mesh->loadMesh("models/triangle.obj");
-
+	// setup the GUI
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
 	GraphicUserInterface gui(&windowManager, glMajVersion, glMinVersion);
 
-	mesh->translate( glm::vec3(0.f, 0.f, -5.0f) );
-	// mesh->rotate( glm::vec3(0.f, 0.f, 30.f) );
-	// mesh->scaleUp( glm::vec3(-0.5f, 0.f, 0.f) );
+	// load a model
+	BasicMesh* mesh = new BasicMesh();
+	mesh->loadMesh("models/Crate1.obj");
+	mesh->translate( glm::vec3(0.f, 0.f, -5.f) );
+
+	float delta = 0.001f;
 
 	while ( windowManager.isOpen() ) {
 		
-		mesh->rotate( glm::vec3(0.f, 0.f, 30.f*0.001f) );
-		// mesh->rotate( glm::vec3(0.f, 30.f*0.001f, 0.f) );
+		mesh->rotate( glm::vec3(0.f, 0.f, 30.f*delta) );
 
 		windowManager.processInput();
 
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 
 		mesh->render(&s);
 
@@ -60,6 +56,20 @@ int main() {
 	}
 
 	return 0;
+}
+
+void configOpenGL(){
+
+	// Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+	// Specify the depth function
+	glDepthFunc(GL_LESS);
+	
+	// Face culling
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CW);
+	glCullFace(GL_BACK);
+
 }
 
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
